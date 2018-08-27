480004186e906efb22d163e46464cca4fff21307
chen <wanjun@chenxuwendeimac.local>
2018-08-27 15:25:20 +0800

    Initial Commit

---
+++ wkltest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  wkltest
+//
+//  Created by 万俊 on 2018/8/27.
+//  Copyright © 2018年 kpwkl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ wkltestTests/wkltestTests.m
@@ -0,0 +1,39 @@
+//
+//  wkltestTests.m
+//  wkltestTests
+//
+//  Created by 万俊 on 2018/8/27.
+//  Copyright © 2018年 kpwkl. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface wkltestTests : XCTestCase
+
+@end
+
+@implementation wkltestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
